cff00c299c6349416724c3097eb38efcac94c90f
Staale Andre <staaleas@gmail.com>
2016-04-23 21:30:39 +0200

    Added support for file location as argument and error handling on open()

--- webScraper.py
+++ webScraper.py
@@ -1,10 +1,8 @@
-# Created by Ståle Andre
-# Lasted modified 23/04/2016 
-
 from lxml import html
 from datetime import datetime
 import requests
 import boto3
+import sys
 
 # Gets info about two search terms; "datavarehus" and "business intelligence"
 datavarehus = requests.get('http://m.finn.no/job/fulltime/search.html?q=datavarehus&industry=65&industry=8&industry=34&sort=1')
@@ -29,12 +27,22 @@ businessIntelligence_stillinger = businessIntelligence_count[0];
 businessIntelligence_annonser = businessIntelligence_count[1];
 businessIntelligenceTekst = "business intelligence,"+businessIntelligence_stillinger+","+businessIntelligence_annonser+","+date
 
+# Gets the file location as an argument
+if len(sys.argv) != 2:
+    sys.exit('Usage: .../webScraper.py fileLocation') # If a file location hasent been passed as an argument
+else:
+	fileLocation = sys.argv[1]
+
 # Appends the file with the new data
-file = open('/Users/staaleas/Documents/Prosjekt/Python/stillinger.csv',"a")
-file.write(datavarehusTekst +"\r\n")
-file.write(businessIntelligenceTekst+"\r\n")
-file.close()
+try:
+	file = open(fileLocation,"a")
+except IOError:
+        print('Cannot open file. Check the that you have the correct file location', arg)
+else:
+	file.write(datavarehusTekst +"\r\n")
+	file.write(businessIntelligenceTekst+"\r\n")
+	file.close()
 
 # Upload the file to S3
 s3_client = boto3.client('s3')
-s3_client.upload_file('/Users/staaleas/Documents/Prosjekt/Python/stillinger.csv', 'samedia', 'stillinger.csv')
+s3_client.upload_file(fileLocation, 'samedia', 'stillinger.csv')
